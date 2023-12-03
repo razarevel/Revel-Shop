45953cfad6552c484849aa2185db169c9316be4c
razarevel <razarevel@gmail.com>
2023-12-03 19:36:52 +0500

    first commit

---
+++ Revel Shop Backend/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log("App is listening to port 3000"));

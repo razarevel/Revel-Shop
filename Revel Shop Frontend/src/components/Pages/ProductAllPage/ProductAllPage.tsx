0dfab9aa841c3a50097c9bc50232cee05391044b
razarevel <razarevel@gmail.com>
2023-12-12 16:03:37 +0500

    updated product detail page

--- Revel Shop Frontend/src/components/Pages/ProductAllPage/ProductAllPage.tsx
+++ Revel Shop Frontend/src/components/Pages/ProductAllPage/ProductAllPage.tsx
@@ -13,6 +13,12 @@ export default function ProductAllPage() {
       <Add_To_Cart setShow={() => setShow(!show)} show={!show} />
       <NavbarDesktop setShow={() => setShow(!show)} />
       <NavbarMobile setShowCart={() => setShow(!show)} />
+      <div className="w-full text-center pt-12 space-y-3">
+        <h1 className="font-bold text-4xl">All Products We Have To Offer</h1>
+        <p className="italic opacity-70 font-medium">
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </p>
+      </div>
       <div>
         <SliderSection For="men" limit={10} />
         <SliderSection For="women" limit={10} />

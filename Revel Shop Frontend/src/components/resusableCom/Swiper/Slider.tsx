9ca2ffbf05f48f0b4f7cb9cf2261cbd95bf50ea8
razarevel <you@example.com>
2024-01-30 17:07:37 +0500

    local backend

--- Revel Shop Frontend/src/components/resusableCom/Swiper/Slider.tsx
+++ Revel Shop Frontend/src/components/resusableCom/Swiper/Slider.tsx
@@ -24,7 +24,7 @@ export default function SliderSection({ For, limit }: Props) {
     useEffect(() => {
       axios
         .get(
-          `http://revel-shop.eu-west-2.elasticbeanstalk.com/api/${For}/?limit=${limit}`
+          `http://127.0.0.1:3000/api//${For}/?limit=${limit}`
         )
         .then((res) => {
           if (For === "men") return setData(res.data.data.men);
--- Revel Shop Frontend/src/components/resusableCom/apiClient.ts
+++ Revel Shop Frontend/src/components/resusableCom/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://revel-shop.eu-west-2.elasticbeanstalk.com/api",
+  baseURL: "http://127.0.0.1:3000/api",
   headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 });
 export default apiClient;
